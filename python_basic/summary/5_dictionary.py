c50ea96f8130e541ea0a604d71796f17621bc327
Oh Donggeon <donggeon.dev@gmail.com>
2021-12-15 00:03:48 +0900

    파이썬 문법 요약에서 딕셔너리의 setdefault() 메서드 설명 오류 수정

--- python_basic/summary/5_dictionary.py
+++ python_basic/summary/5_dictionary.py
@@ -3,7 +3,7 @@ dictionary = {1: 'Hi', 2: 'Hello'}
 print(dictionary[2])  # 만약 인덱스로 없는 key에 접근하게 된다면 런타임 오류 발생
 print(dictionary.get(2))  # None을 출력하고 런타임 오류가 발생하지 않는다
 # 2에 해당하는 value가 없으면 Bonjour를 추가하고 있다면 해당 value를 가져온다.
-print(dictionary.get(2, 'Bonjour'))
+print(dictionary.setdefault(2, 'Bonjour'))
 print(3 in dictionary)  # False, key 값이 3인 value가 있는지 묻는 것
 print(dictionary.keys())  # 모든 key 값
 print(dictionary.values())  # 모든 value 값
