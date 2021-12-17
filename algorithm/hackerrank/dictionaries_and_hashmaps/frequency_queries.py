1001912087e664d7a388810b4ef780870e3fbfe9
Oh Donggeon <donggeon.dev@gmail.com>
2021-12-17 16:20:44 +0900

    frequency_queries.py 솔루션 주석 추가

--- algorithm/hackerrank/dictionaries_and_hashmaps/frequency_queries.py
+++ algorithm/hackerrank/dictionaries_and_hashmaps/frequency_queries.py
@@ -2,6 +2,15 @@ from collections import defaultdict
 
 
 def freq_query(queries):
+    """
+    해커랭크 Frequency queries 솔루션
+
+    문제의 핵심 관건은 query[0] == 2 인 경우에
+    query[1] 과 같은 빈도 수를 가진 value를 찾는 것이였는데,
+    value를 key로 가지고 빈도수를 value로 가지는 dict로 찾기엔 O(n)이 걸리므로
+    또 다른 frequency를 key로 가지고 frequency의 빈도를 value로 가지는 dict를 하나 더 생성하여
+    빈도수를 찾는데 O(1) 이 걸리도록 하는게 핵심
+    """
     value_dict = defaultdict(int)
     frequency_dict = defaultdict(int)
 
