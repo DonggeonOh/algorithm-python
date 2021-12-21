a7005c33b45ea600d28197968353c3d185fcb088
Oh Donggeon <donggeon.dev@gmail.com>
2021-12-22 00:36:39 +0900

    해커랭크 Time complexity: Primality 솔루션 추가

---
+++ python_basic/miscellaneous/time_complexity_primality.py
@@ -0,0 +1,14 @@
+def primality(n):
+    prime_str = "Prime"
+    not_prime_str = "Not prime"
+
+    if n == 1:
+        return not_prime_str
+    elif n % 2 == 0 and n != 2:
+        return not_prime_str
+
+    for num in range(3, int(sqrt(n) + 1), 2):
+        if n % num == 0 and n != num:
+            return not_prime_str
+
+    return prime_str
