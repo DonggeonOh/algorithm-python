ded70c258048e6a64ada401a25b52298c26fa869
Oh Donggeon <donggeon.dev@gmail.com>
2021-12-28 17:03:21 +0900

    삽입 정렬 알고리즘 구현

---
+++ data_structure/sortings/insert_sort.py
@@ -0,0 +1,11 @@
+def insert_sort(values):
+    for swapindex in range(len(values)):
+        minindex = swapindex
+
+        for targetindex in range(swapindex + 1, len(values)):
+            if values[minindex] > values[targetindex]:
+                minindex = targetindex
+
+        values[minindex], values[swapindex] = values[swapindex], values[minindex]
+
+    return values
