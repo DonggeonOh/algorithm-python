6614e4364d6b1109c541382f7bdf4721f2f00897
Oh Donggeon <donggeon.dev@gmail.com>
2021-12-28 19:32:58 +0900

    삽입정렬 알고리즘 수정 및 시간복잡도 최적화

--- algorithm/sortings/insert_sort.py
+++ algorithm/sortings/insert_sort.py
@@ -1,15 +1,7 @@
 def insert_sort(values):
     for targetindex in range(1, len(values)):
-        if values[targetindex] >= values[targetindex - 1]:
-            print(values[targetindex])
-            continue
-
-        insertindex = targetindex
-
-        for sortedindex in range(targetindex - 1, -1, -1):
-            if values[sortedindex] > values[targetindex]:
-                insertindex = sortedindex
+        for sortedindex in range(targetindex, 0, -1):
+            if values[sortedindex] < values[sortedindex - 1]:
+                values[sortedindex], values[sortedindex - 1] = values[sortedindex - 1], values[sortedindex]
             else:
                 break
-
-        values.insert(insertindex, values.pop(targetindex))
