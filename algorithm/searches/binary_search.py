b5cede294f18480f32d8333a3c40ee52b7be85d0
Oh Donggeon <donggeon.dev@gmail.com>
2021-12-29 16:45:19 +0900

    이진탐색 알고리즘 구현 코드 추가

---
+++ algorithm/searches/binary_search.py
@@ -0,0 +1,25 @@
+from math import ceil
+
+
+def binary_search(seq, target):
+    """
+    이진 탐색은 정렬되어 있는 값여야 수행 가능하다.
+
+    :param seq: 정렬 되어있는 Sequence
+    :param target: input 에서 찾으려고 하는 값
+    :return target이 위치한 index 반환
+    """
+    low = 0
+    high = len(seq) - 1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+
+        if target < seq[mid]:
+            high = mid - 1
+        elif target > seq[mid]:
+            low = mid + 1
+        else:
+            return mid
+
+    return -1
