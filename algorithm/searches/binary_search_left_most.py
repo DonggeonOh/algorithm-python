d31cdf0488d32c1f1c956b085e19239973e2d6f0
Oh Donggeon <donggeon.dev@gmail.com>
2021-12-29 22:25:14 +0900

    이진탐색 중복된 값에 대한 인덱스를 반환 할 때 가장 왼쪽 인덱스를 반환하는 이진 탐색 알고리즘 추가

---
+++ algorithm/searches/binary_search_left_most.py
@@ -0,0 +1,23 @@
+def binary_search_left_most(seq, target):
+    """
+    중복된 값이 있는 경우의 가장 왼쪽 인덱스를 반환하는 이진탐색
+
+    :param seq: 정렬 되어있는 Sequence
+    :param target: input 에서 찾으려고 하는 값
+    :return target이 위치한 index 반환
+    """
+    if not seq:
+        return None
+
+    low = 0
+    high = len(seq) - 1
+
+    while low < high:
+        mid = int((low + high) / 2)
+
+        if seq[mid] < target:
+            low = mid + 1
+        else:
+            high = mid
+
+    return low if seq[low] == target else None
