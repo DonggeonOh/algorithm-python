a77e810b1feaca28d1c587c239e05e4d3f8b9c0e
Oh Donggeon <donggeon.dev@gmail.com>
2021-12-29 16:45:54 +0900

    이진탐색 최적화 대안 알고리즘 추가

--- algorithm/searches/binary_search.py
+++ algorithm/searches/binary_search.py
@@ -23,3 +23,28 @@ def binary_search(seq, target):
             return mid
 
     return -1
+
+
+def binary_search_optimize(seq, target):
+    """
+    위 이진탐색에서 비교문을 줄여 최적화한 이진탐색 알고리즘이다.
+
+    :param seq: 정렬 되어있는 Sequence
+    :param target: input 에서 찾으려고 하는 값
+    :return target이 위치한 index 반환
+    """
+    low = 0
+    high = len(seq) - 1
+
+    while low < high:
+        mid = ceil((low + high) / 2)
+
+        if target < seq[mid]:
+            high = mid - 1
+        else:
+            low = mid
+
+    if seq[low] == target:
+        return low
+    else:
+        return -1
