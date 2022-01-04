008be31361a2eba45b3406626b506fae9bc4e130
Oh Donggeon <donggeon.dev@gmail.com>
2022-01-04 20:11:06 +0900

    다익스트라 heapq 최적화 코드 작성

--- algorithm/shortest_path/dijkstra.py
+++ algorithm/shortest_path/dijkstra.py
@@ -1,4 +1,5 @@
 from sys import maxsize
+from heapq import *
 
 
 def dijkstra(graph, start, end):
@@ -27,3 +28,28 @@ def min_distance_vertex(iter, visited):
             min_index = index
 
     return None if min_value == maxsize else min_index
+
+
+def dijkstra_min_heap(graph, start, end):
+    shortest_distance = init_shortest_distance_tuple_list(graph.len)
+    visited = set()
+
+    heapify(shortest_distance)
+
+    for _ in range(graph.len):
+        min_vertex = heappop(shortest_distance)
+        visited.add(min_vertex[1])
+
+        for vertex, edge in enumerate(graph.edges(min_vertex)):
+            if edge and shortest_distance[min_vertex[0]][0] + edge < shortest_distance[vertex][0]:
+                shortest_distance[vertex][0] = (shortest_distance[min_vertex[0]][0] + edge, vertex)
+
+    return None if shortest_distance[end] == maxsize else shortest_distance[end]
+
+
+def init_shortest_distance_tuple_list(len):
+    result = [(maxsize, index) for index in range(len)]
+
+    result[0] = (0, 0)
+
+    return result
