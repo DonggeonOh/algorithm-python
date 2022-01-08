68008b41f270af4f5b906e8dfae2f901f81eadc5
Oh Donggeon <donggeon.dev@gmail.com>
2022-01-09 02:01:19 +0900

    유니온 파인드 find 메서드 수정

--- algorithm/union_find/union_find.py
+++ algorithm/union_find/union_find.py
@@ -1,4 +1,7 @@
 def union_parent(table, a, b):
+    """
+    두 집합간의 parent도 같이 바뀌어야 하는데 바뀌지 않아 다시 짤 필요가 있음
+    """
     union_a = find_parent(table, a)
     union_b = find_parent(table, b)
 
@@ -9,7 +12,14 @@ def union_parent(table, a, b):
 
 
 def find_parent(table, index):
+    stack = list()
+
     while table[index] != index:
+        stack.append(index)
         index = table[index]
 
-    return table[index]
+    while stack:
+        table_index = stack.pop()
+        table[table_index] = index
+
+    return index
