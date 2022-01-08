b0f1fd97ac279c3854256d280c8f9b040530c66c
Oh Donggeon <donggeon.dev@gmail.com>
2022-01-08 23:48:45 +0900

    위상정렬 시간복잡도 설명 주석 추가

--- algorithm/union_find/topological_sorting.py
+++ algorithm/union_find/topological_sorting.py
@@ -7,6 +7,9 @@ def topological_sorting(graph):
         방향성에 거스르지 않도록 한마디로 순서를 나열하여 순서대로 나열한 것이다.
         위상정렬의 경우 여러가지의 결과가 나올 수 있다.
         구현의 경우 DFS, Queue 를 이용하여 구현할 수 있고 Queue를 사용하여 구현 하였다.
+        시간복잡도는 진입 정점(Incoming edge)이 0인 정점부터 차례대로 모든 정점을 확인하며
+        각 정점에서 진출 정점(outgoing edge)을 차례대로 제거해야 한다.
+        따라서, 위상 정렬의 시간 복잡도는 O(V + E) 이다. V - Vertex, E - Edge
 
         :param graph: 인접행렬로 구현된 그래프
         :return: 정점의 정렬 순서
