303f8e09b6e1d715edecb6bfd099d6cf558ca551
Oh Donggeon <donggeon.dev@gmail.com>
2022-03-29 15:40:39 +0900

    프로그래머스 모의고사 주석 추가

--- algorithm/problems/programmers/problem/mock_test.py
+++ algorithm/problems/programmers/problem/mock_test.py
@@ -1,4 +1,14 @@
 def mock_test(answers):
+    """
+    프로그래머스 코딩테스트 고득점 Kit 완전탐색 - 모의고사 솔루션
+
+    Author: Oh Donggeon
+    Date: 2022/03/28
+    Link: https://programmers.co.kr/learn/courses/30/lessons/42840
+
+    :param answers: 문제의 답들
+    :return: 가장 많이 맞춘 학생의 순서대로 출력 (같을 경우 오름차순으로 출력)
+    """
     students = [
         ([1, 2, 3, 4, 5], 5),
         ([2, 1, 2, 3, 2, 4, 2, 5], 8),
